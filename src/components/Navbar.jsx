db41cb566b697b151645bcb810aff2de9c5b121c
rob-by-w <rwayong.code@gmail.com>
2023-01-28 16:17:17 +0900

    Enabled responsive navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,32 +14,50 @@ function Navbar() {
 				<a className="navbar-brand" href="/">
 					Bond Auction Date
 				</a>
-				<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-					<li className="nav-item">
-						<a
-							className={"nav-link " + (currentPath === "/" ? "active" : "")}
-							href="/"
-						>
-							Home
-						</a>
-					</li>
-					<li className="nav-item">
-						<a
-							className={"nav-link " + (currentPath === "/jgb" ? "active" : "")}
-							href="/jgb"
-						>
-							JGB
-						</a>
-					</li>
-					<li className="nav-item">
-						<a
-							className={"nav-link " + (currentPath === "/ust" ? "active" : "")}
-							href="/ust"
-						>
-							UST
-						</a>
-					</li>
-				</ul>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a
+								className={"nav-link " + (currentPath === "/" ? "active" : "")}
+								href="/"
+							>
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a
+								className={
+									"nav-link " + (currentPath === "/jgb" ? "active" : "")
+								}
+								href="/jgb"
+							>
+								JGB
+							</a>
+						</li>
+						<li className="nav-item">
+							<a
+								className={
+									"nav-link " + (currentPath === "/ust" ? "active" : "")
+								}
+								href="/ust"
+							>
+								UST
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
