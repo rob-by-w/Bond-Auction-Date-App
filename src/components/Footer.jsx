dbce9738e4eceeab01ca8dd7e657789f6a86c386
rob-by-w <rwayong.code@gmail.com>
2023-01-29 17:31:05 +0900

    Added linkedin and github link in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,27 @@ function Footer() {
 			<div className="container p-4">
 				<div className="row">
 					<div className="col">
-						<span className="align-middle">&#169;{currentYear} Robby Wayong</span>
+						<div className="align-middle">&#169;{currentYear} Robby Wayong</div>
+						<div className="mt-2">
+							<span className="p-3">
+								<a
+									target="_blank"
+									href="https://www.linkedin.com/in/rwayong"
+									className="anchor-icon"
+								>
+									<i class="fa-brands fa-linkedin"></i>
+								</a>
+							</span>
+							<span>
+								<a
+									target="_blank"
+									href="https://github.com/rob-by-w"
+									className="anchor-icon"
+								>
+									<i class="fa-brands fa-square-github"></i>
+								</a>
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
