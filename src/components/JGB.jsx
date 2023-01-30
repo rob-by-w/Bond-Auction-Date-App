0e497f35ef9ef7e42ca70625a83ca598ceb78482
rob-by-w <rwayong.code@gmail.com>
2023-01-30 20:34:44 +0900

    Added loader spinner to JGB.jsx and UST.jsx

--- src/components/JGB.jsx
+++ src/components/JGB.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Bars } from "react-loader-spinner";
 import axios from "axios";
 
 const baseUrl = "http://127.0.0.1:5000";
 
 export default function AuctionDateJgb() {
 	const [bondAuctionDate, setBondAuctionDate] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	const parseNextAuction = (nextAuctionDate) => {
 		const gmtDate = new Date(Date.parse(nextAuctionDate));
@@ -28,6 +30,7 @@ export default function AuctionDateJgb() {
 			} catch (error) {
 				console.log(error.response);
 			}
+			setIsLoading(false);
 		};
 
 		fetchAuctionDate();
@@ -46,41 +49,52 @@ export default function AuctionDateJgb() {
 				</div>
 				<div className="row mt-3">
 					<div className="col-8 mx-auto">
-						<table className="table table-light table-bordered align-middle">
-							<thead className="align-middle">
-								<tr>
-									<th>Tenor</th>
-									<th>Next Auction Date (JST)</th>
-									<th>Bond Number</th>
-									<th>Reopen</th>
-									<th>Announcement Link</th>
-								</tr>
-							</thead>
-							<tbody className="table-group-divider">
-								{bondAuctionDate.map((bond) => {
-									console.log(bond.tenor);
-									return (
-										<tr key={bond.tenor}>
-											<th>{bond.tenor}</th>
-											<td>{parseNextAuction(bond.next_auction)}</td>
-											<td>{bond.bond_number}</td>
-											<td>{bond.reopen_info}</td>
-											<td>
-												{bond.announcement_link && (
-													<a
-														href={bond.announcement_link}
-														target="_blank"
-														rel="noopener noreferrer"
-													>
-														Here
-													</a>
-												)}
-											</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</table>
+						{isLoading ? (
+							<Bars
+								height="8rem"
+								width="8rem"
+								color="#b1b2ff"
+								ariaLabel="bars-loading"
+								wrapperClass="loader-spinner"
+								visible={true}
+							/>
+						) : (
+							<table className="table table-light table-bordered align-middle">
+								<thead className="align-middle">
+									<tr>
+										<th>Tenor</th>
+										<th>Next Auction Date (JST)</th>
+										<th>Bond Number</th>
+										<th>Reopen</th>
+										<th>Announcement Link</th>
+									</tr>
+								</thead>
+								<tbody className="table-group-divider">
+									{bondAuctionDate.map((bond) => {
+										console.log(bond.tenor);
+										return (
+											<tr key={bond.tenor}>
+												<th>{bond.tenor}</th>
+												<td>{parseNextAuction(bond.next_auction)}</td>
+												<td>{bond.bond_number}</td>
+												<td>{bond.reopen_info}</td>
+												<td>
+													{bond.announcement_link && (
+														<a
+															href={bond.announcement_link}
+															target="_blank"
+															rel="noopener noreferrer"
+														>
+															Here
+														</a>
+													)}
+												</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+						)}
 					</div>
 				</div>
 			</div>
--- src/components/UST.jsx
+++ src/components/UST.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Bars } from "react-loader-spinner";
 import axios from "axios";
 
 const baseUrl = "http://127.0.0.1:5000";
 
 export default function AuctionDateUst() {
 	const [bondAuctionDate, setBondAuctionDate] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	const parseNextAuction = (nextAuctionDate) => {
 		const gmtDate = new Date(Date.parse(nextAuctionDate));
@@ -21,13 +23,14 @@ export default function AuctionDateUst() {
 		const fetchAuctionDate = async () => {
 			const url = baseUrl + "/ust-auction-date";
 			try {
-				const {data} = await axios.get(url);
+				const { data } = await axios.get(url);
 
 				console.log(data);
 				setBondAuctionDate(data);
 			} catch (error) {
 				console.log(error.response);
 			}
+			setIsLoading(false);
 		};
 
 		fetchAuctionDate();
@@ -46,31 +49,42 @@ export default function AuctionDateUst() {
 				</div>
 				<div className="row mt-3">
 					<div className="col-8 mx-auto">
-						<table className="table table-light table-bordered align-middle">
-							<thead className="align-middle">
-								<tr>
-									<th>Tenor</th>
-									<th>Next Auction Date (ET)</th>
-									<th>Announcement Date (ET)</th>
-									<th>Settlement Date (ET)</th>
-									<th>Reopen</th>
-								</tr>
-							</thead>
-							<tbody className="table-group-divider">
-								{bondAuctionDate.map((bond) => {
-									console.log(bond.security_type);
-									return (
-										<tr key={bond.security_type}>
-											<th>{bond.security_type}</th>
-											<td>{parseNextAuction(bond.auction_date)}</td>
-											<td>{parseNextAuction(bond.announcement_date)}</td>
-											<td>{parseNextAuction(bond.settlement_date)}</td>
-											<td>{bond.reopen_info}</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</table>
+						{isLoading ? (
+							<Bars
+								height="8rem"
+								width="10rem"
+								color="#b1b2ff"
+								ariaLabel="bars-loading"
+								wrapperClass="loader-spinner"
+								visible={true}
+							/>
+						) : (
+							<table className="table table-light table-bordered align-middle">
+								<thead className="align-middle">
+									<tr>
+										<th>Tenor</th>
+										<th>Next Auction Date (ET)</th>
+										<th>Announcement Date (ET)</th>
+										<th>Settlement Date (ET)</th>
+										<th>Reopen</th>
+									</tr>
+								</thead>
+								<tbody className="table-group-divider">
+									{bondAuctionDate.map((bond) => {
+										console.log(bond.security_type);
+										return (
+											<tr key={bond.security_type}>
+												<th>{bond.security_type}</th>
+												<td>{parseNextAuction(bond.auction_date)}</td>
+												<td>{parseNextAuction(bond.announcement_date)}</td>
+												<td>{parseNextAuction(bond.settlement_date)}</td>
+												<td>{bond.reopen_info}</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+						)}
 					</div>
 				</div>
 			</div>
