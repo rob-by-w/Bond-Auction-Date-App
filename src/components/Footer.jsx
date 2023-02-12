cb2f993a4801fc41f54ba13f2f6380f90df68756
rob-by-w <rwayong.code@gmail.com>
2023-02-12 12:42:57 +0900

    Enabled full screen for multiple screen size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,35 +4,70 @@ function Footer() {
 	let currentYear = new Date().getFullYear();
 
 	return (
-		<footer className="footer text-center">
-			<div className="container p-4">
-				<div className="row">
-					<div className="col">
-						<div className="align-middle">&#169;{currentYear} Robby Wayong</div>
-						<div className="mt-2">
-							<span className="p-3">
-								<a
-									target="_blank"
-									href="https://www.linkedin.com/in/rwayong"
-									className="anchor-icon"
-								>
-									<i class="fa-brands fa-linkedin"></i>
-								</a>
-							</span>
-							<span>
-								<a
-									target="_blank"
-									href="https://github.com/rob-by-w"
-									className="anchor-icon"
-								>
-									<i class="fa-brands fa-square-github"></i>
-								</a>
-							</span>
+		<div>
+			<footer className="footer text-center fixed-bottom show-on-long">
+				<div className="container p-4">
+					<div className="row">
+						<div className="col">
+							<div className="align-middle">
+								&#169;{currentYear} Robby Wayong
+							</div>
+							<div className="mt-2">
+								<span className="p-3">
+									<a
+										target="_blank"
+										href="https://www.linkedin.com/in/rwayong"
+										className="anchor-icon"
+									>
+										<i class="fa-brands fa-linkedin"></i>
+									</a>
+								</span>
+								<span>
+									<a
+										target="_blank"
+										href="https://github.com/rob-by-w"
+										className="anchor-icon"
+									>
+										<i class="fa-brands fa-square-github"></i>
+									</a>
+								</span>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</footer>
+			</footer>
+			<footer className="footer text-center show-on-short">
+				<div className="container p-4">
+					<div className="row">
+						<div className="col">
+							<div className="align-middle">
+								&#169;{currentYear} Robby Wayong
+							</div>
+							<div className="mt-2">
+								<span className="p-3">
+									<a
+										target="_blank"
+										href="https://www.linkedin.com/in/rwayong"
+										className="anchor-icon"
+									>
+										<i class="fa-brands fa-linkedin"></i>
+									</a>
+								</span>
+								<span>
+									<a
+										target="_blank"
+										href="https://github.com/rob-by-w"
+										className="anchor-icon"
+									>
+										<i class="fa-brands fa-square-github"></i>
+									</a>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
 	);
 }
 
